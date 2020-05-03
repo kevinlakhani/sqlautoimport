5cdcc27aaf7a55a72ef6eaf4f969301212c0bae4
kevinlakhani <kevinlakhani@gmail.com>
2020-05-03 13:57:48 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# sqlautoimport
- Tool for creating/populating/replacing SQL tables from CSV files
+
+# SQL Auto Import (sqlautoimport)
+Welcome to the SQL Auto Import tool. This command-line tool, written in Python® allows you to quickly create/populate or replace tables in a SQL database with CSV files.
+
+PostreSQL, MySQL, SQLite, MSSQL, and Oracle dialects are supported.
+
+This tool will attempt to load connection settings from a config.json file by default. You can specify
+a file to use as a command line argument. If no file is found or if there is an issue, you will be asked
+to input the information below. You will have the options to save the connection settings. 
+
+Additionally, you will need to specify a directory path to your CSV files (e.g. C:\MyFiles). You can
+also pass this in as a second command-line argument. 
+
+If you do not enter a path, the local directory will be used.
+
+This tool will use the names of your files as table names (in lowercase), so make sure they're correct. If a table already exists, it will be replaced.
+
+Only the default schema is currently supported.
+
+# Usage
+The source Python file (.py) is provided, which can be used on any system where Python is installed. For Windows users who do not wish to use or install Python, an executable file (.exe) is provided.
+
+Using the .exe file requires Microsoft Visual C++ to be installed on your system.
+
+The when using the .py or .exe file, two arguments may be passed:
+
+ 1. The file path of the config file containing connection details
+ 2. The folder path of the CSV files to be imported to your database
+
+If these arguments are not passed or if there is an issue with them, you will be prompted to enter those details.
+
+# Attributions (L
+SQL Auto Import (sqlautoimport) is uses the following open-source libraries, each of which have their own copyright and license:
+
+ - **pandas**
+	 - License: [https://github.com/pandas-dev/pandas/blob/master/LICENSE](https://github.com/pandas-dev/pandas/blob/master/LICENSE)
+	 - Copyright
+		 - Copyright (c) 2008-2011, AQR Capital Management, LLC, Lambda Foundry, Inc. and PyData Development Team. All rights reserved.
+		 - Copyright (c) 2011-2020, Open source contributors.
+ - **sqlalchemy**
+	 - License [https://github.com/sqlalchemy/sqlalchemy/blob/master/LICENSE](https://github.com/sqlalchemy/sqlalchemy/blob/master/LICENSE)
+	 - Copyright:
+		 - Copyright 2005-2020 SQLAlchemy authors and contributors \<see AUTHORS file>.
+- **tqdm**
+	- License: [https://github.com/tqdm/tqdm/blob/master/LICENCE](https://github.com/tqdm/tqdm/blob/master/LICENCE)
+	- Copyright:
+		- MPLv2.0 2015-2020 (c) Casper da Costa-Luis
+		- MIT 2016 (c) [PR #96] on behalf of Google Inc.
+		- MIT 2013 (c) Noam Yorav-Raphael, original author.
+- **psycopg2**
+	 - License: 
+		 - [https://github.com/psycopg/psycopg2/blob/master/LICENSE](https://github.com/psycopg/psycopg2/blob/master/LICENSE)
+		 - [https://www.psycopg.org/license/](https://www.psycopg.org/license/)
+	 - Copyright:
+		 - ?
+ -  **sqlite3**
+	 - License: [https://www.sqlite.org/copyright.html](https://www.sqlite.org/copyright.html)
+	 - Copyright:
+		 - Public Domain
+- **pyodbc**
+	- License: [https://github.com/mkleehammer/pyodbc/blob/master/LICENSE.txt](https://github.com/mkleehammer/pyodbc/blob/master/LICENSE.txt)
+- **cx_Oracle**
+	- License: [https://github.com/oracle/python-cx_Oracle/blob/master/LICENSE.txt](https://github.com/oracle/python-cx_Oracle/blob/master/LICENSE.txt)
+	- Copyright: 
+		- Copyright 2016, 2018, Oracle and/or its affiliates. All rights reserved.
+		- Portions Copyright 2007-2015, Anthony Tuininga. All rights reserved.
+		- Portions Copyright 2001-2007, Computronix (Canada) Ltd., Edmonton, Alberta, Canada. All rights reserved.
+- **mysqlclient**:
+	- License: [https://github.com/PyMySQL/mysqlclient-python/blob/master/LICENSE](https://github.com/PyMySQL/mysqlclient-python/blob/master/LICENSE)
+- **os**, **time**, **json**, **sys**, and any other Python built-in libraries:
+	- License: [https://wiki.python.org/moin/PythonSoftwareFoundationLicenseFaq](https://wiki.python.org/moin/PythonSoftwareFoundationLicenseFaq) 
+
+"Python" is a registered trademark of the Python Software Foundation
+
+# Roadmap
+ - Support for schema specification
+ - Support for .json files
+ - Support for specifying modes other than "replace" (e.g. "append")
---
+++ sqlautoimport.py
@@ -0,0 +1,176 @@
+import pandas, sqlalchemy, tqdm, os, time, json, sys
+# https://docs.sqlalchemy.org/en/13/dialects/
+import psycopg2, sqlite3, cx_Oracle, pyodbc
+
+def chunker(seq, size):
+    # Thanks to miraculixx
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+config_path, csv_path = 'config.json',''
+if len(sys.argv) > 1:
+    config_path = sys.argv[1] 
+    if len(sys.argv) > 2:
+        csv_path = sys.argv[2]
+
+print('''
+    Welcome to the SQL Auto Import tool. This tool
+    allows you to quickly create/populate or replace 
+    tables in a SQL database with CSV files.
+
+    PostreSQL, MySQL, SQLite, MSSQL, and Oracle dialects
+    are supported.
+
+    This tool will attempt to load connection settings
+    from a config.json file by default. You can specify
+    a file to use as a command line argument. If no file
+    is found or if there is an issue, you will be asked
+    to input the information below. You will have the 
+    options to save the connection settings. 
+
+    Additionally, you will need to specify a directory 
+    path to your CSV files (e.g. C:\MyFiles). You can
+    also pass this in as a second command-line argument. 
+    
+    If you do not enter a path, the local directory will 
+    be used.
+
+    This tool will use the names of your files as table 
+    names (in lowercase), so make sure they're correct. 
+    If a table already exists, it will be replaced.
+
+    Only the default schema is currently supported.
+'''
+)
+
+dialect, host, port, database, password = ('' for i in range(5))
+
+settings = {'dialect': '', 'host': '', 'database': '', 'port': '', 'username': '', 'password': ''}
+try:
+    with open(config_path) as config_file:
+        config = json.load(config_file)
+
+    settings['dialect'] = config['dialect']
+    settings['host'] = config['host']
+    settings['port'] = config['port']
+    settings['database'] = config['database']
+    settings['username'] = config['username']
+    settings['password'] = config['password']
+except Exception as e:
+    print ('No config.json file found or there was an issue loading the configuration.')
+    print(e)
+    print()
+
+if any(settings[x] == '' for x in settings.keys()):
+    print('Please enter the SQL connection settings prompted below.')
+    for x in settings.keys():
+        print()
+        hint = ''
+        if x == 'dialect':
+            hint = ' (e.g. postgres, mysql, mssql, oracle)'
+        if x == 'port':
+            hint = ' (usually 5432 for postgres or 3306 for mysql)'
+        if settings[x] == '':
+            settings[x] = input(f'{x.capitalize()}{hint}: ')
+    print()
+    save = input('Would you like to save these settings for next time [y/n]? ').lower()
+    if save in ['y','yes']:
+        new_config = '{\n'
+        for k,v in settings.items():
+            new_config += f'\t"{k}":"{v}"'
+            if k != list(settings.keys())[-1]: 
+                new_config += ','
+            new_config += '\n'
+        new_config += '}'
+
+        with open('config.json','w') as config_file:
+            config_file.write(new_config)
+            print('config.json saved succefully')
+            print()
+dialect = ''
+if settings['dialect'].lower() in ['postgres','postgresql']:
+    dialect = 'postgres'
+elif settings['dialect'].lower() == 'mysql':
+    dialect = 'mysql+mysqldb'
+elif settings['dialect'].lower() == 'sqlite':
+    dialect = 'sqlite+pysqlite'
+elif settings['dialect'].lower() == 'oracle':
+    dialect = 'oracle+cx_oracle'
+elif settings['dialect'].lower() == 'mssql':
+    dialect = 'mssql+pyodbc'
+
+
+uri = f"{dialect}://{settings['username']}:{settings['password']}@{settings['host']}:{settings['port']}/{settings['database']}"
+print()
+print('The following connection URI will be used:')
+print(uri)
+print()
+engine = sqlalchemy.create_engine(uri)
+
+if csv_path == '':
+    csv_path = input('Directory Path: ')
+
+csv_files,file_list = [],[]
+
+if csv_path == '':
+    file_list = os.listdir()
+else:
+    file_list = os.listdir(csv_path)
+
+for f in file_list:
+    if f.endswith(".csv"):
+        csv_files.append(os.path.join(csv_path, f))
+
+#test = pandas.read_sql('select * from baseline_cognitive_data', engine)
+#print(test)
+if len(csv_files) > 0:
+    confirm = input(f'''
+This will import {str(len(csv_files))} file(s) into the specified database. 
+Any tables that already exists will be overwritten.
+Continue? [y/n]: '''
+    ).lower()
+    if confirm in ['y','yes']:
+        for x in range(0,len(csv_files)):
+            print()
+            print(f'Reading {str(csv_files[x])}...')
+            df = None
+            try:
+                df = pandas.read_csv(csv_files[x])
+            except Exception as e:
+                print(f'Error reading file: {str(csv_files[x])}')
+                print(e)
+
+            try:    
+                print(f"Importing {str(csv_files[x])} to database as {csv_files[x][:csv_files[x].index('.csv')].lower()}...")
+                print(f'Rows to insert: {(len(df.index))}')
+                
+                start = time.perf_counter()
+                block = int(len(df) / 10)
+                with tqdm.tqdm(total=len(df)) as progress:
+                    for i, cdf in enumerate(chunker(df, block)):
+                        mode = "replace" if i == 0 else "append"
+                        cdf.to_sql(
+                            csv_files[x][:csv_files[x].index('.csv')].lower(),
+                            engine,
+                            index = False,
+                            if_exists = mode,
+                            # schema = 'default', #TO DO: arguments and/or settings file
+                            method = 'multi', 
+                            chunksize = 10000
+                            )            
+                        progress.update(block)
+                        tqdm.tqdm._instances.clear()
+
+                finish = time.perf_counter()
+                print(f'''Completed importing {str(x+1)} out of {len(csv_files)} file(s).
+Total time: {finish - start:0.4f} seconds.
+Seconds per row: {(finish - start)/(len(df.index)):0.4f}
+''')
+            except Exception as e:
+                print(e)
+        print()
+        print('Finished!')
+else:
+    print('No CSV files found in the specified directory.')
+
+print()
+input('Press any key to exit.')
